b119cb568ccdfec56fe22da339aac83a024aafb2
Arkachu <108960010+Arkachu@users.noreply.github.com>
2022-07-09 09:03:35 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
+/* Bouton pour lancer une partie */
 let boutonJouer = document.querySelector('#boutonJouer');
+
+/* Bouton pour retourner au menu principal */
 let boutonRetour = document.querySelector('#boutonRetour');
 boutonRetour.hidden = true;
 
+/* Boutons de sélection du nombre de joueurs */
 let bouton1 = document.querySelector('#bouton1');
 bouton1.hidden = true;
 let bouton2 = document.querySelector('#bouton2');
@@ -27,14 +31,18 @@ bouton11.hidden = true;
 let bouton12 = document.querySelector('#bouton12');
 bouton12.hidden = true;
 
+/* Titre de la page */
 let titre = document.querySelector('h1');
 
+/* Paragraphes présents sur la page */
 let paragraphe1 = document.querySelector('#paragraphe1');
 let paragraphe2 = document.querySelector('#paragraphe2');
 let paragraphe3 = document.querySelector('#paragraphe3');
 
+/* Liste de la page */
 let liste = document.querySelector('ul');
 
+/* Eléments de la liste */
 let li1 = document.querySelector('#li1');
 let li2 = document.querySelector('#li2');
 let li3 = document.querySelector('#li3');
@@ -48,6 +56,7 @@ let li10 = document.querySelector('#li10');
 let li11 = document.querySelector('#li11');
 let li12 = document.querySelector('#li12');
 
+/* Ajout d'une action au bouton jouer */
 boutonJouer.addEventListener('click', function() {
     titre.textContent = 'Choix du nombre de joueurs';
     paragraphe1.textContent = '';
@@ -70,6 +79,7 @@ boutonJouer.addEventListener('click', function() {
     boutonRetour.hidden = false;
 });
 
+/* Ajout d'une action au bouton retour */
 boutonRetour.addEventListener('click', function() {
     titre.textContent = 'Jeu Kobaladream'
     paragraphe1.textContent = "Vous êtes actuellement sur la page d'accueil du jeu Kobaladream. Ce jeu est un jeu d'alcool qui vous permettra d'incarner un des personnages suivants :";
@@ -102,3 +112,469 @@ boutonRetour.addEventListener('click', function() {
     bouton12.hidden = true;
     boutonRetour.hidden = true;
 })
+
+/* Nombre de joueurs dans la partie */
+let nbJoueurs;
+
+/* Joueur actuel */
+let Joueur;
+
+/* Personnages sélectionnés par les joueurs */
+let persoJoueur1;
+let persoJoueur2;
+let persoJoueur3;
+let persoJoueur4;
+let persoJoueur5;
+let persoJoueur6;
+let persoJoueur7;
+let persoJoueur8;
+let persoJoueur9;
+let persoJoueur10;
+let persoJoueur11;
+let persoJoueur12;
+
+/* Boutons de sélection des personnages */
+let boutonAnat = document.querySelector('#boutonAnat');
+boutonAnat.hidden = true;
+let boutonAntho = document.querySelector('#boutonAntho');
+boutonAntho.hidden = true;
+let boutonMathis = document.querySelector('#boutonMathis');
+boutonMathis.hidden = true;
+let boutonDoume = document.querySelector('#boutonDoume');
+boutonDoume.hidden = true;
+let boutonLoic = document.querySelector('#boutonLoic');
+boutonLoic.hidden = true;
+let boutonThib = document.querySelector('#boutonThib');
+boutonThib.hidden = true;
+let boutonKelvin = document.querySelector('#boutonKelvin');
+boutonKelvin.hidden = true;
+let boutonNico = document.querySelector('#boutonNico');
+boutonNico.hidden = true;
+let boutonLeo = document.querySelector('#boutonLeo');
+boutonLeo.hidden = true;
+let boutonLucien = document.querySelector('#boutonLucien');
+boutonLucien.hidden = true;
+let boutonGaby = document.querySelector('#boutonGaby');
+boutonGaby.hidden = true;
+let boutonEttore = document.querySelector('#boutonEttore');
+boutonEttore.hidden = true;
+
+/* Bouton retour au menu de sélection du nombre de joueurs */
+let boutonRetourJoueurs = document.querySelector('#boutonRetourJoueurs');
+boutonRetourJoueurs.hidden = true;
+
+/* Ajout d'une action au bouton1 */
+bouton1.addEventListener('click', function() {
+    nbJoueurs = 1;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton2 */
+bouton2.addEventListener('click', function() {
+    nbJoueurs = 2;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton3 */
+bouton3.addEventListener('click', function() {
+    nbJoueurs = 3;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton4 */
+bouton4.addEventListener('click', function() {
+    nbJoueurs = 4;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton5 */
+bouton5.addEventListener('click', function() {
+    nbJoueurs = 5;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton6 */
+bouton6.addEventListener('click', function() {
+    nbJoueurs = 6;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton7 */
+bouton7.addEventListener('click', function() {
+    nbJoueurs = 7;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton8 */
+bouton8.addEventListener('click', function() {
+    nbJoueurs = 8;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton9 */
+bouton9.addEventListener('click', function() {
+    nbJoueurs = 9;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton10 */
+bouton12.addEventListener('click', function() {
+    nbJoueurs = 10;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton11 */
+bouton11.addEventListener('click', function() {
+    nbJoueurs = 11;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+/* Ajout d'une action au bouton12 */
+bouton12.addEventListener('click', function() {
+    nbJoueurs = 12;
+    Joueur = 1;
+    titre.textContent = 'Choix des personnages : joueur 1'
+    bouton1.hidden = true;
+    bouton2.hidden = true;
+    bouton3.hidden = true;
+    bouton4.hidden = true;
+    bouton5.hidden = true;
+    bouton6.hidden = true;
+    bouton7.hidden = true;
+    bouton8.hidden = true;
+    bouton9.hidden = true;
+    bouton10.hidden = true;
+    bouton11.hidden = true;
+    bouton12.hidden = true;
+    boutonRetour.hidden = true;
+    boutonAnat.hidden = false;
+    boutonAntho.hidden = false;
+    boutonMathis.hidden = false;
+    boutonDoume.hidden = false;
+    boutonLoic.hidden = false;
+    boutonThib.hidden = false;
+    boutonKelvin.hidden = false;
+    boutonNico.hidden = false;
+    boutonLeo.hidden = false;
+    boutonLucien.hidden = false;
+    boutonGaby.hidden = false;
+    boutonEttore.hidden = false;
+    boutonRetourJoueurs.hidden = false;
+})
+
+/* Ajout d'une action au bouton retour joueurs */
+boutonRetourJoueurs.addEventListener('click', function() {
+    titre.textContent = 'Choix du nombre de joueurs';
+    bouton1.hidden = false;
+    bouton2.hidden = false;
+    bouton3.hidden = false;
+    bouton4.hidden = false;
+    bouton5.hidden = false;
+    bouton6.hidden = false;
+    bouton7.hidden = false;
+    bouton8.hidden = false;
+    bouton9.hidden = false;
+    bouton10.hidden = false;
+    bouton11.hidden = false;
+    bouton12.hidden = false;
+    boutonRetour.hidden = false;
+    boutonAnat.hidden = true;
+    boutonAntho.hidden = true;
+    boutonMathis.hidden = true;
+    boutonDoume.hidden = true;
+    boutonLoic.hidden = true;
+    boutonThib.hidden = true;
+    boutonKelvin.hidden = true;
+    boutonNico.hidden = true;
+    boutonLeo.hidden = true;
+    boutonLucien.hidden = true;
+    boutonGaby.hidden = true;
+    boutonEttore.hidden = true;
+    boutonRetourJoueurs.hidden = true;
+});
